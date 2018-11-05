5cf405c81b1ab01f1348688a0f369723b12602ff
Antonio J. Nebro <antonio@lcc.uma.es>
2018-11-05 17:05:35 +0100

    Add class PairwiseReplacement (pending to be tested)

---
+++ src/main/java/org/uma/moore/component/evolutionaryalgorithm/selection/impl/DifferentialEvolutionSelection.java
@@ -0,0 +1,62 @@
+package org.uma.moore.component.evolutionaryalgorithm.selection.impl;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.comparator.DominanceComparator;
+import org.uma.moore.Message;
+import org.uma.moore.component.evolutionaryalgorithm.selection.Selection;
+import org.uma.moore.util.DataBuffer;
+
+/**
+ * Applies a binary tournament selection to return the best solution between two that have been
+ * chosen at random from a solution list.
+ * Modified by Juanjo in 13.03.2015. A binary tournament is now a TournamenteSelection with 2 
+ * tournaments
+ *
+ * @author Antonio J. Nebro, Juan J. Durillo
+ */
+
+public class DifferentialEvolutionSelection extends Selection<DoubleSolution> {
+  private org.uma.jmetal.operator.impl.selection.DifferentialEvolutionSelection differentialEvolutionSelection ;
+  private int matingPoolSize;
+
+  public DifferentialEvolutionSelection(int matingPoolSize) {
+    super("Binary tournament selection") ;
+    this.differentialEvolutionSelection =
+        new org.uma.jmetal.operator.impl.selection.DifferentialEvolutionSelection() ;
+    this.matingPoolSize = matingPoolSize ;
+  }
+
+  @Override
+  public void onNext(Message message) {
+    if (!(boolean)message.getAttribute("ALGORITHM_TERMINATED")) {
+      List<DoubleSolution> population = (List<DoubleSolution>) message.getAttribute("POPULATION");
+
+      List<DoubleSolution> newPopulation = new ArrayList<>(matingPoolSize) ;
+      for (int i = 0; i < population.size(); i++) {
+        // Obtain parents. Two parameters are required: the population and the
+        //                 index of the current individual
+        differentialEvolutionSelection.setIndex(i);
+        List<DoubleSolution> parents = differentialEvolutionSelection.execute(population);
+
+        newPopulation.addAll(parents);
+      }
+      message.setAttribute("MATING_POOL", newPopulation);
+    }
+
+    getObservable().setChanged();
+    getObservable().notifyObservers(message);
+  }
+
+  @Override
+  public void onFinish(Message message) {
+  }
+
+  @Override
+  public String getDescription() {
+    return "Binary tournament selection object";
+  }
+}
